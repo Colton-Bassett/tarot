4d3a80b54df53596647bf983cdd3bc7a77163edd
Colton Bassett <coltonbassett@gmail.com>
2025-01-08 14:52:23 -0800

    Added basic header, footer, disclaimer page templates. Added prettier tailwind plugin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from './Footer.svelte';
+	import Header from './Header.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<!-- {@render children()} -->
+
+<div class="app">
+	<Header />
+	<main class="min-h-screen p-8">
+		{@render children()}
+	</main>
+	<Footer />
+</div>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,23 @@
+<footer class="flex justify-between px-8">
+	<div class="corner flex h-16 items-center">
+		<a href="/">cb.tarot</a>
+	</div>
+
+	<nav>
+		<ul class="flex gap-6">
+			<li class="flex h-16 items-center">
+				<a href="/">home</a>
+			</li>
+			<li class="flex h-16 items-center">
+				<a href="/disclaimer">disclaimer</a>
+			</li>
+			<li class="flex h-16 items-center">
+				<a href="/other">other</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="corner flex h-16 items-center">
+		<a href="https://github.com/sveltejs/kit"> gh </a>
+	</div>
+</footer>
